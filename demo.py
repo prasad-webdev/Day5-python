d20064941bb1ab4a9cfc16ad6fb3e8808fdac071
Prasad Bairagi <prasadbairagi131@gmail.com>
2026-09-17 14:34:29 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,3 +1,25 @@
-def greet():
-    print("hello, World!")
-greet()
+# def greet():
+#     print("hello, World!")
+# greet()
+
+
+# def add(a,b):
+#     print("sum : ",a+b)
+#     print(a)
+#     print(b)
+# add(10,5)
+
+# def pi():
+#     return 3.14
+# print(pi())                               #x=pi()   print(x)
+
+
+# def greet(name="student"):
+#     print("hello",name)
+# greet()
+# greet("Prasad")
+# greet("Pranav")
+
+def square(n):
+    return n*n
+print(square(int(input("enter a number: "))))
